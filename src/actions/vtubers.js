1e1a70ec2df45ee9b76b7337a9f0fb151d2b7e2e
Code4Cat <tongkaitik1997@gmail.com>
2021-04-22 05:52:49 +0800

    Bruh

--- src/actions/vtubers.js
+++ src/actions/vtubers.js
@@ -1,22 +1,23 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
 import * as api from '../api/index.js';
 
 //Action Creators
 export const getVtubers = () => async (dispatch) => {
-    try{
-        const { data } = await api.fetchVtubers();
-        dispatch({ type: 'FETCH_ALL', payload: data });
-    } catch(err){
-        console.log(err.message);
-    }
-    
+  try {
+    const { data } = await api.fetchVtubers();
+    dispatch({ type: FETCH_ALL, payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+
 }
 
 export const createVtuber = (vtuber) => async (dispatch) => {
-    try {
-      const { data } = await api.createVtuber(vtuber);
-  
-      dispatch({ type: 'CREATE', payload: data });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  try {
+    const { data } = await api.createVtuber(vtuber);
+
+    dispatch({ type: CREATE, payload: data });
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/reducers/vtubers.js
+++ src/reducers/vtubers.js
@@ -1,10 +1,26 @@
-export default (vtubers = [],action) => {
-    switch(action.type){
-        case 'FETCH_ALL':
-            return action.payload;
-        case 'CREATE':
-            return vtubers;
-        default:
-            return vtubers;
-    }
-}
+// export default (vtubers = [],action) => {
+//     switch(action.type){
+//         case 'FETCH_ALL':
+//             return action.payload;
+//         case 'CREATE':
+//             return vtubers;
+//         default:
+//             return vtubers;
+//     }
+// }
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
+
+export default (vtubers = [], action) => {
+  switch (action.type) {
+    case FETCH_ALL:
+      return action.payload;
+    case CREATE:
+      return [...vtubers, action.payload];
+    // case UPDATE:
+    //   return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
+    // case DELETE:
+    //   return posts.filter((post) => post._id !== action.payload);
+    default:
+      return vtubers;
+  }
+};
