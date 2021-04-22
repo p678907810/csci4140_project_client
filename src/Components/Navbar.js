fe0574647e5d4cec427a06115068d8c34b02bc0c
Code4Cat <tongkaitik1997@gmail.com>
2021-04-22 16:49:20 +0800

    Update favicon

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,22 +1,24 @@
 import React from 'react'
-import {Link} from 'react-router-dom';
-import {LinkContainer} from 'react-router-bootstrap';
-import {Navbar,Button,Form,Nav,FormControl} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Navbar, Button, Form, Nav, FormControl } from 'react-bootstrap';
 const NavBar = () => {
-    return(
-    <nav>
-    <Navbar bg="dark" variant="dark">
-        <Navbar.Brand>VMusic</Navbar.Brand>
-        <Nav className="mr-auto">
-            <LinkContainer to="/"><Nav.Link>Home</Nav.Link></LinkContainer>
-            <LinkContainer to="/explore"><Nav.Link>Explore</Nav.Link></LinkContainer>
-            <LinkContainer to="/addVTuber"><Nav.Link>Add Vtuber</Nav.Link></LinkContainer>
-            <LinkContainer to="/addSong"><Nav.Link>Add Song</Nav.Link></LinkContainer>
-        </Nav>
-        
-    </Navbar>
-    </nav>  
-     
+    return (
+        <nav>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Navbar.Brand>VMusic</Navbar.Brand>
+                    <Nav className="mr-auto">
+                        <LinkContainer to="/"><Nav.Link>Home</Nav.Link></LinkContainer>
+                        <LinkContainer to="/explore"><Nav.Link>Explore</Nav.Link></LinkContainer>
+                        <LinkContainer to="/addVTuber"><Nav.Link>Add Vtuber</Nav.Link></LinkContainer>
+                        <LinkContainer to="/addSong"><Nav.Link>Add Song</Nav.Link></LinkContainer>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </nav>
+
     )
 }
 export default NavBar;
